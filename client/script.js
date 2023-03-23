c8256301d6cd33b95bb8b645c34786cf45495996
Joshua <joshuacompton@my.smccd.edu>
2023-03-23 00:11:09 +0000

    dvferwf

--- client/script.js
+++ client/script.js
@@ -1,6 +1,53 @@
-const kissMyBtn = document.querySelector("#buttON");
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
-kissMyBtn.addEventListener("click", function() {
-  const kwestShun = document.getElementById("typedText").value;
-  document.getElementById("chat_container").innerHTML = kwestShun;
-});
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+    element.textContent ='';
+
+    loadInterval = setInterval(() => {
+        element.textContent += '.';
+
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0;
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerText += text.charAt(index);
+            index++;    
+        } else {
+            clearInterval(interval);
+        }
+    }, 20)
+}
+
+function generateUniqueId(){
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${ isAi && 'ai' }">
+            <div class="chat">
+                <div class="profile">
+                    <img src="" alt=""
+                </div>
+            </div>
+        </div>
+        `
+    )
+}
